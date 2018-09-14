ce740255aa487a80cbea299a70221f2147b7f76a
robmachado <linux.rlm@gmail.com>
2018-09-14 17:44:06 -0300

    [UPD] ajustes

--- src/serialcomm.js
+++ src/serialcomm.js
@@ -15,6 +15,7 @@ function read(callback) {
     parser.on('data', (data) => {
         var res = data.split(" ");
         var pB = res[1].replace(/[^0-9.,]+/g, "");
+        pB.replace(',', '.');
         //console.log(pB)
         callback(pB)
     })
